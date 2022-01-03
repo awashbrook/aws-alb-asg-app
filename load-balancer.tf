d364b24c7ab04503fadd3c8af64b923e84dfd7fe
awashbrook <awashbrook@gmail.com>
2022-01-03 00:10:21 +0000

    Adding ASG Target  Group and Listener

--- load-balancer.tf
+++ load-balancer.tf
@@ -1,28 +1,48 @@
 resource "aws_lb" "my-alb" {
-  name            = "my-alb"
-  internal        = false
+  name               = "my-alb"
+  internal           = false
   load_balancer_type = "application"
-  subnets         = [aws_subnet.main-public-1.id, aws_subnet.main-public-2.id]
-  security_groups = [aws_security_group.lb-securitygroup.id]
-  # listener {
-  #   instance_port     = 80
-  #   instance_protocol = "http"
-  #   lb_port           = 80
-  #   lb_protocol       = "http"
-  # }
-  # health_check {
-  #   healthy_threshold   = 2
-  #   unhealthy_threshold = 2
-  #   timeout             = 3
-  #   target              = "HTTP:80/"
-  #   interval            = 30
-  # }
-
-  # cross_zone_load_balancing   = true
-  # connection_draining         = true
-  # connection_draining_timeout = 400
+  subnets            = [aws_subnet.main-public-1.id, aws_subnet.main-public-2.id]
+  security_groups    = [aws_security_group.lb-securitygroup.id]
   tags = {
     Name = "my-alb"
   }
 }
+resource "aws_lb_listener" "my-lb-listener" {
+  load_balancer_arn = aws_lb.my-alb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my-lb-tg.arn
+  }
+  #  listener {
+  #   instance_port     = 80
+  #   instance_protocol = "http"
+  #   lb_port           = 80
+  #   lb_protocol       = "http"
+  # } 
+}
+resource "aws_lb_target_group" "my-lb-tg" {
+  name     = "App-TargetGroup"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    # target              = "HTTP:80/"
+    protocol = "HTTP"
+    port     = 80
+    interval = 30
+    // TODO
+    # path = "/index.html"
+    # matcher = "200,202"
+  }
 
+  # cross_zone_load_balancing   = true
+  # connection_draining         = true
+  # connection_draining_timeout = 400
+
+}
