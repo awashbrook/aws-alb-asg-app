f2966eb9e008a3f246805bbcc4b5cb5fb7a104f4
awashbrook <awashbrook@gmail.com>
2022-01-03 22:01:41 +0000

    Trouble After initial deploy 502 Bad Gateway

--- auto-scaling.tf
+++ auto-scaling.tf
@@ -1,10 +1,12 @@
 resource "aws_launch_configuration" "example-launchconfig" {
-  name_prefix     = "example-launchconfig"
-  image_id        = var.AMIS[var.AWS_REGION]
-  instance_type   = "t3.nano"
-  key_name        = aws_key_pair.mykeypair.key_name
-  security_groups = [aws_security_group.myinstance.id]
-  user_data       = "#!/bin/bash\napt-get update\napt-get -y install net-tools nginx\nMYIP=`ifconfig | grep -E '(inet 10)|(addr:10)' | awk '{ print $2 }' | cut -d ':' -f2`\necho 'this is: '$MYIP > /var/www/html/index.html"
+  name_prefix                 = "example-launchconfig"
+  associate_public_ip_address = true # TODO Only for troubleshooting
+  image_id                    = data.aws_ami.amazon_linux_2.image_id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.mykeypair.key_name
+  security_groups             = [aws_security_group.myinstance.id]
+  user_data                   = filebase64("scripts/init_webserver.sh")
+  # user_data       = "#!/bin/bash\napt-get update\napt-get -y install net-tools nginx\nMYIP=`ifconfig | grep -E '(inet 10)|(addr:10)' | awk '{ print $2 }' | cut -d ':' -f2`\necho 'this is: '$MYIP > /var/www/html/index.html"
   lifecycle {
     create_before_destroy = true
   }
@@ -12,7 +14,7 @@ resource "aws_launch_configuration" "example-launchconfig" {
 
 resource "aws_autoscaling_group" "example-autoscaling" {
   name                      = "example-autoscaling"
-  vpc_zone_identifier       = [aws_subnet.main-public-1.id, aws_subnet.main-public-2.id]
+  vpc_zone_identifier       = [aws_subnet.main-public-1.id, aws_subnet.main-public-2.id] # TODO Make Private
   launch_configuration      = aws_launch_configuration.example-launchconfig.name
   min_size                  = 2
   max_size                  = 2
